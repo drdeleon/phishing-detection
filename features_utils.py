ba9d873e554a084582e6a5cc9a93dcf5fac25e4e
Douglas De León <dele18037@uvg.edu.gt>
2022-01-23 16:22:23 -0600

    Feature generation

--- features_utils.py
+++ features_utils.py
@@ -1,113 +1,129 @@
+import numbers
 from urllib.parse import urlparse
+import re
 
 
-def f1(url: str):
+def f1(url: str) -> int:
     """ Full URL length. """
 
     return len(url)
 
-def f2(url: str):
+def f2(url: str) -> int:
     """ Hostname length. """
 
-    return "{uri.netloc}".format(uri=urlparse(url))
+    hostname = "{uri.netloc}".format(uri=urlparse(url))
 
-def f4(url: str):
+    return len(hostname)
+
+def f4(url: str) -> int:
     """ Number of occurences of character '.' in URL. """
 
     return url.count('.')
 
-def f5(url: str):
+def f5(url: str) -> int:
     """ Number of occurences of character '-' in URL. """
 
     return url.count('-')
 
-def f6(url: str):
+def f6(url: str) -> int:
     """ Number of occurences of character '@' in URL. """
 
     return url.count('@')
 
-def f7(url: str):
+def f7(url: str) -> int:
     """ Number of occurences of character '?' in URL. """
 
     return url.count('?')
 
-def f8(url: str):
+def f8(url: str) -> int:
     """ Number of occurences of character '&' in URL. """
 
     return url.count('&')
 
-def f9(url: str):
+def f9(url: str) -> int:
     """ Number of occurences of character '|' in URL. """
 
     return url.count('|')
 
-def f10(url: str):
+def f10(url: str) -> int:
     """ Number of occurences of character '=' in URL. """
 
     return url.count('=')
 
-def f11(url: str):
+def f11(url: str) -> int:
     """ Number of occurences of character '_' in URL. """
 
     return url.count('_')
 
-def f12(url: str):
+def f12(url: str) -> int:
     """ Number of occurences of character '~' in URL. """
 
     return url.count('~')
 
-def f13(url: str):
+def f13(url: str) -> int:
     """ Number of occurences of character '%' in URL. """
 
     return url.count('%')
 
-def f14(url: str):
+def f14(url: str) -> int:
     """ Number of occurences of character '/' in URL. """
 
     return url.count('/')
 
-def f15(url: str):
+def f15(url: str) -> int:
     """ Number of occurences of character '*' in URL. """
 
     return url.count('*')
 
-def f16(url: str):
+def f16(url: str) -> int:
     """ Number of occurences of character ':' in URL. """
 
     return url.count(':')
 
-def f17(url: str):
+def f17(url: str) -> int:
     """ Number of occurences of character ',' in URL. """
 
     return url.count(',')
 
-def f18(url: str):
+def f18(url: str) -> int:
     """ Number of occurences of character ';' in URL. """
 
     return url.count(';')
 
-def f19(url: str):
+def f19(url: str) -> int:
     """ Number of occurences of character '$' in URL. """
 
     return url.count('$')
 
-def f20(url: str):
+def f20(url: str) -> int:
     """ Number of occurences of character '%20' or space in URL. """
 
     return url.count(' ')
 
-def f25(url: str):
+def f25(url: str) -> bool:
     """ Checks for HTTPS token in URL. """
     # TODO HTTPS token
     
-    uri = urlparse(url)
+    scheme = urlparse(url).scheme
+    return scheme == 'https'
 
-def f26(url: str):
+def f26(url: str) -> float:
     """ Ratio of digits in full URL. """
-    
-    return 
 
-def f27(url: str):
-    """ Ratio of digits in hostname. """
+    if (len(url) == 0):
+        return 0.0
+
+    numeric = len(re.sub("[^0-9]", "", url))
     
-    return
+    return numeric / len(url)
+
+def f27(url: str) -> float:
+    """ Ratio of digits in hostname. """
+
+    if (len(url) == 0):
+        return 0.0
+
+    hostname = urlparse(url).netloc
+    numbers = len(re.sub("[^0-9]", "", hostname))
+    
+    return numbers / len(hostname)
